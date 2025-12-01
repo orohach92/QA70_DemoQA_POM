ef773b4a1524a579ee0ff17dc413b637c9261e5a
orohach92 <rohach.oleksandr@gmail.com>
2025-12-01 11:48:35 +0100

    initial commit

--- src/main/java/com/demoqa/pages/SidePanel.java
+++ src/main/java/com/demoqa/pages/SidePanel.java
@@ -1,10 +1,19 @@
 package com.demoqa.pages;
 
 import com.demoqa.core.BasePage;
+import com.demoqa.pages.bookstore.LoginPage;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class SidePanel extends BasePage {
     public SidePanel(WebDriver driver) {
         super(driver);
+    }
+@FindBy(xpath = "//span[.='Login']")
+    WebElement login;
+    public LoginPage selectLogin() {
+        click(login);
+        return new LoginPage(driver);
     }
 }
--- src/main/java/com/demoqa/pages/bookstore/LoginPage.java
+++ src/main/java/com/demoqa/pages/bookstore/LoginPage.java
@@ -2,10 +2,27 @@ package com.demoqa.pages.bookstore;
 
 import com.demoqa.core.BasePage;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class LoginPage extends BasePage {
 
     public LoginPage(WebDriver driver) {
         super(driver);
+    }
+@FindBy(id = "userName")
+    WebElement userNameInput;
+    @FindBy(id = "password")
+    WebElement passwordInput;
+    public LoginPage enterUserData(String name, String passwort) {
+        type(userNameInput,name);
+        type(passwordInput,passwort);
+        return this;
+    }
+@FindBy(id = "login")
+WebElement loginButton;
+    public ProfilePage clickOnLoginButton() {
+        click(loginButton);
+        return new ProfilePage(driver);
     }
 }
